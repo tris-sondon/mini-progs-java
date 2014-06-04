c6b4f3fb5f928f491622a0bff1de8307e705c70e
Tristana Sondon <tristana.sondon@gmail.com>
2014-06-04 13:32:54 +0200

    Added Hola

---
+++ group_01/Hola.java
@@ -0,0 +1,9 @@
+public class Hola
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hola ");
+        System.out.print(args[0]);
+        System.out.println(". Como va?");
+    }
+}
