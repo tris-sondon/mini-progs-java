48030d480300c333e6c4eb4b334988b989c5496c
Tristana Sondon <tristana.sondon@gmail.com>
2014-06-04 14:22:59 +0200

    Addeed Hola3Names

--- README.rst
+++ README.rst
@@ -3,5 +3,6 @@ mini-progs-java
 
 * HolaMundo
 * Hola (uses command line argument)
-* ej1: HolaMundo10 (no loops)
+* ex1: HolaMundo10 (no loops)
+* ex2: Hola3Names (takes 3 names as args, says hi in reversed order). 
 * 
---
+++ group_01/Hola3Names.java
@@ -0,0 +1,16 @@
+// Takes 3 names as arguments, and writes them in 
+// reversed order.
+
+public class Hola3Names
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hola ");
+        System.out.print(args[2]);
+        System.out.print(", ");
+        System.out.print(args[1]);
+        System.out.print(" and ");
+        System.out.print(args[0]);
+        System.out.println("!");
+    }
+}
