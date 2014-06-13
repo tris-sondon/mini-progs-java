45824f7035284f4ae9a4c36fd4ff4952daefe5b4
Tristana Sondon <tristana.sondon@gmail.com>
2014-06-13 21:57:50 +0200

    Added Curva

---
+++ group_04_drawings/Curva.java
@@ -0,0 +1,13 @@
+public class Curva 
+{
+    public static void main (String [] args)
+    {
+        int N = Integer.parseInt(args[0]);
+        StdDraw.setXscale(0, N);
+        StdDraw.setYscale(0, N);
+        for (int i = 0; i <= N; i++) 
+        {
+            StdDraw.line(0, N-i, i, 0);    
+        }
+    }
+}
