76088bbb277cf700b4d508e7ca033ab021780ca8
Tristana Sondon <tristana.sondon@gmail.com>
2014-06-15 16:50:51 +0200

    Added SqrtByNewton

---
+++ group_06_functions_and_methods/SqrtByNewton.java
@@ -0,0 +1,41 @@
+// This code calculates the square root of a number
+// using Newton's Method, up to 15 decimal places.
+// Refactoring of the initial code, now it does:
+//      - If c is negative, returns NaN
+//      - divided user interaction and sqrt calculations in functions
+//      - Can take several numbers from command line, for easy testing.
+//
+
+public class SqrtByNewton 
+{
+    public static double sqrt(double c)
+    {
+        if (c < 0) return Double.NaN;
+        double epsilon = 1e-15;
+        double t = c; // initial estimate
+
+        while (Math.abs(t - c/t) > epsilon * t)
+        {
+            t = (c/t + t) / 2.0;    
+        }
+        return t;
+    }
+
+
+    public static void main(String[] args)
+    {
+        int N = args.length;
+        double[] num = new double[N];
+        for (int i = 0; i < N; i++) 
+        {
+            num[i] = Double.parseDouble(args[i]);
+            System.out.println(num[i]);
+        }
+        System.out.println();
+        for (int i = 0; i < N; i++) 
+        {
+            double s = sqrt(num[i]);
+            System.out.println(s);
+        }
+    }
+}
